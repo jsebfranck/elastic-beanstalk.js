8991f7f19f4205e1d3d51fe07b9348296caf1e77
Jean-Sébastien Franck <jsebfranck@gmail.com>
2015-02-10 17:19:31 +0100

    Test of version creation and deployment

--- test/elastic-beanstalk.test.js
+++ test/elastic-beanstalk.test.js
@@ -23,6 +23,45 @@ describe('Elastic Beanstalk client', function() {
     });
   });
 
+  it('should create a version then deploy', function() {
+    elasticBeanstalk.awsClient = {
+      uploadArchiveToS3: function(fileName, remoteFilename) {
+        expect(fileName).to.equal('FILENAME');
+        expect(remoteFilename).to.equal('REMOTE-FILENAME');
+
+        return Q.resolve();
+      },
+      createApplicationVersion: function(versionLabel, description, remoteFilename) {
+        expect(versionLabel).to.equal('VERSION_LABEL');
+        expect(description).to.equal('DESCRIPTION');
+        expect(remoteFilename).to.equal('REMOTE-FILENAME');
+
+        return Q.resolve({
+          versionLabel: 'VERSION_ID'
+        });
+      },
+      updateEnvironmentVersion: function(environmentName, newVersion) {
+        expect(environmentName).to.equal('TARGET');
+        expect(newVersion).to.equal('VERSION_ID');
+
+        return Q.resolve();
+      },
+      waitEnvironmentToBeReady: function(environmentName) {
+        expect(environmentName).to.equal('TARGET');
+
+        return Q.resolve();
+      }
+    };
+
+    elasticBeanstalk.createVersionAndDeploy({
+      environment: 'TARGET',
+      filename: 'FILENAME',
+      remoteFilename: 'REMOTE-FILENAME',
+      versionLabel: 'VERSION_LABEL',
+      description: 'DESCRIPTION'
+    }).done();
+  });
+
   it('should promote a version', function() {
     elasticBeanstalk.awsClient = {
       getEnvironmentInfo: function(environmentName) {
