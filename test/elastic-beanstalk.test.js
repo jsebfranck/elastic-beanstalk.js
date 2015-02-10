13f388b53fa351f2b02d9ea839ffca722a2d7c05
Jean-Sébastien Franck <jsebfranck@gmail.com>
2015-02-10 17:40:03 +0100

    Added test on version promotion with newrelic

--- test/elastic-beanstalk.test.js
+++ test/elastic-beanstalk.test.js
@@ -10,15 +10,26 @@ describe('Elastic Beanstalk client', function() {
   nock.disableNetConnect();
 
   var elasticBeanstalk;
+  var elasticBeanstalkWithNewRelicConfig;
 
   beforeEach(function() {
+    var awsConfig = {
+      accessKeyId: 'ACCESS_KEY_ID',
+      secretAccessKey: 'SECRET_ACCESS_KEY',
+      region: 'REGION',
+      applicationName: 'APPLICATION_NAME',
+      versionsBuckets: 'VERSIONS_BUCKETS'
+    };
+
     elasticBeanstalk = new ElasticBeanstalk({
-      aws: {
-        accessKeyId: 'ACCESS_KEY_ID',
-        secretAccessKey: 'SECRET_ACCESS_KEY',
-        region: 'REGION',
-        applicationName: 'APPLICATION_NAME',
-        versionsBuckets: 'VERSIONS_BUCKETS'
+      aws: awsConfig
+    });
+
+    elasticBeanstalkWithNewRelicConfig = new ElasticBeanstalk({
+      aws: awsConfig,
+      newrelic: {
+        applicationName: 'NR_APP',
+        applicationKey: 'NR_KEY'
       }
     });
   });
@@ -85,6 +96,41 @@ describe('Elastic Beanstalk client', function() {
     };
 
     elasticBeanstalk.promoteVersion({
+      sourceEnvironment: 'SOURCE',
+      targetEnvironment: 'TARGET'
+    }).done();
+  });
+
+
+  it('should promote a version and notify new relic', function() {
+    elasticBeanstalk.awsClient = {
+      getEnvironmentInfo: function(environmentName) {
+        expect(environmentName).to.equal('SOURCE');
+
+        return Q.resolve({
+          version: 'VERSION_LABEL'
+        });
+      },
+      updateEnvironmentVersion: function(environmentName, newVersion) {
+        expect(environmentName).to.equal('TARGET');
+        expect(newVersion).to.equal('VERSION_LABEL');
+
+        return Q.resolve();
+      },
+      waitEnvironmentToBeReady: function(environmentName) {
+        expect(environmentName).to.equal('TARGET');
+
+        return Q.resolve();
+      }
+    };
+
+    elasticBeanstalk.newrelicClient = {
+      notifyNewrelicApp: function() {
+        return Q.resolve();
+      }
+    };
+
+    elasticBeanstalkWithNewRelicConfig.promoteVersion({
       sourceEnvironment: 'SOURCE',
       targetEnvironment: 'TARGET'
     }).done();
